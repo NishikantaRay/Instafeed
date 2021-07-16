315de138720ebc201606373cb9c8169f36eaa8a5
Nishikanta Ray <62615392+NishikantaRay@users.noreply.github.com>
2021-07-16 23:02:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var userFeed = new Instafeed({
     target: "instafeed-container",
     resolution: 'low_resolution',
     limit:30,
-    accessToken: 'IGQVJWWWJ4dUg3QUpLMjV1MDNEWEFXdnp4c285QWVkXzRtdUc5dklwQ2RKaWRBcTBVYjYwcWJVQlNyU2lvUUtZAOFJld3g1SHdKOVcxdlhKa0ZAlcE1aZAkNXSHcyZAWM1NjJyblNRNUIxZA2tfbU5RNUNOQgZDZD'
+    accessToken: ''
 });
 userFeed.run();
 
